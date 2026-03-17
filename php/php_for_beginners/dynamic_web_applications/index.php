3f2a94b1e5256c0209a3180d1f0a8459cbff81d2
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-17 16:41:29 +0200

    PDO first steps lesson

--- php/php_for_beginners/dynamic_web_applications/index.php
+++ php/php_for_beginners/dynamic_web_applications/index.php
@@ -1,5 +1,18 @@
 <?php
-
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
 
+//connect to our MySQL database.
+$dsn = "mysql:host=localhost:3306;dbname=dev_training_demo;charset=utf8mb4;password=secret";
+
+$pdo = new PDO($dsn, 'user');
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post["title"] . "</li>";
+}
