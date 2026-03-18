327ad2cf986f8b310b45ae4b0651d042e5b28964
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-18 11:55:57 +0200

    add basic authorization to note page

--- php/php_for_beginners/notes_mini_project/controllers/note.php
+++ php/php_for_beginners/notes_mini_project/controllers/note.php
@@ -5,6 +5,18 @@ $db = new Database($config['database']);
 
 $heading = "My Notes";
 
-$note = $db->query('SELECT * from notes where id = :id', [':id' => $_GET['id']])->fetch();
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [  
+    ':id' => $_GET['id']
+])->fetch();
+
+if(!$note) {
+    abort();
+}
+
+$currentUserId = 1;
+
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+} 
 
 require "views/note.view.php"; 
