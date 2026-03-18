cbd1d2b2632d50ba565ba6129563381014cbfa28
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-18 10:24:50 +0200

    added in files from previous section

---
+++ php/php_for_beginners/notes_mini_project/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+//connect to our MySQL database and execute a query.
+class Database {
+
+    public $connection;
+
+    public function __construct($config, $username = "user", $password = "secret")
+    {
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $params = [])
+    {
+        $statement = $this->connection->prepare($query);
+
+        $statement->execute($params);
+
+        return $statement;
+    }
+}
---
+++ php/php_for_beginners/notes_mini_project/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+'database' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'dbname' => 'notes-mini-project',
+        'charset' => 'utf8mb4',
+        'user' => 'user',
+        'password' => 'secret'
+    ],
+];
