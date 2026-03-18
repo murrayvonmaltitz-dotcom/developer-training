d2c1ba48de3b22214b0748aad0971f91c962ea98
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-18 15:19:03 +0200

    added validator class

--- php/php_for_beginners/notes_mini_project/views/note-create.view.php
+++ php/php_for_beginners/notes_mini_project/views/note-create.view.php
@@ -22,9 +22,9 @@
                                         rows="3"
                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                         placeholder="Here's an idea for a note..."
-                                        required
+                                    
                                     >
-                                        <?= $_POST['body'] ?? "" ?>
+                                        <?= $_POST['body'] ?? '' ?>
                                     </textarea>
 
                                     <?php if (isset($errors['body'])) : ?>
