63c7d7566c88dc989d1256ebebb80b1c7006b812
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-19 15:15:41 +0200

    created and implemented db srvice container

---
+++ php/php_for_beginners/project-organization/Core/App.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Core;
+
+class App {
+    protected static $container;
+
+    public static function setContainer($container) {
+        static::$container = $container;
+    }
+    
+    public static function container() {
+        return static::$container;
+    }
+
+    public static function bind($key, $resolve) {
+        dd($resolve);
+        static::container()->bind($key, $resolve);
+    }
+
+    public static function resolve($key) {
+        return static::container()->resolve($key);
+    }
+}
--- php/php_for_beginners/project-organization/public/index.php
+++ php/php_for_beginners/project-organization/public/index.php
@@ -5,11 +5,12 @@ const BASE_PATH = __DIR__.'/../';
 require BASE_PATH.'Core/functions.php';
 
 spl_autoload_register(function ($class) {
-    // Core\Database
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     require base_path("{$class}.php");
 });
+
+require base_path('bootstrap.php');
 
 $router = new Core\Router();
 $routes = require base_path("routes.php");
