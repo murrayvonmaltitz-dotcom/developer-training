a5c1d28d85231c20c61033202db6e5dcc563dffc
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-24 10:21:06 +0200

    removed unnessasary reset function

--- javascript/guessing_game/script.js
+++ javascript/guessing_game/script.js
@@ -1,23 +1,14 @@
 
-//Object Basics
-
-
-//basic object creation
-let person = {
-    firstName: "John",
-    lastName: "Doe",
-    getFullName() {
-        return `${this.firstName} ${this.lastName}`
-    }
-}
+//Object Parameters
 
 const game = (function() {
-    const secretNumber = Math.floor(Math.random() * 10) + 1
-    const maxAttempts = 3
-    const history = [];
 
     return {
         play() {
+            const secretNumber = Math.floor(Math.random() * 10) + 1
+            const maxAttempts = 3
+            const history = [];
+
             while (history.length < maxAttempts) {
             var input = prompt(`Attempt ${history.length + 1}: Guess the secret number between 1 and 10`);
             var guess = Number(input)
@@ -49,14 +40,7 @@ const game = (function() {
         alert(`Game over! The secret number was ${secretNumber}. You ${guessedMessage} in ${history.length} attempts.`);
         alert(`Your guesses were: ${history.join(', ')}`);
         },
-        reset() {
-            const secretNumber = Math.floor(Math.random() * 10) + 1
-            history.length = 0
-        },
     }
 })()
 
-//play game twice and reset in between
-game.play();
-game.reset();
 game.play();
