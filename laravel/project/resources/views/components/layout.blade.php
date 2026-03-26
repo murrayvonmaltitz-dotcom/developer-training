88aa9bb6e34babd2e097e725c9b1c35553caff3c
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-26 13:15:15 +0200

    added styling with daisyui

--- laravel/project/resources/views/components/layout.blade.php
+++ laravel/project/resources/views/components/layout.blade.php
@@ -3,20 +3,23 @@
 ])
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
     <style>
         nav > a {
             color: blue;
         }
     </style>
 </head>
-<body class="bg-gray-700 p-6 max-w-xl mx-auto text-white">
-    <main>
+<body class="">
+<x-nav />
+    <main class="max-w-3xl mx-auto">
         {{ $slot }}
     </main>
 </body>
--- laravel/project/resources/views/ideas/index.blade.php
+++ laravel/project/resources/views/ideas/index.blade.php
@@ -3,9 +3,13 @@
     @if ($ideas->count())
         <div class="mt-6 text-white">
             <h2 class="font-bold">Your Ideas</h2>
-            <ul class="mt-6">
+            <ul class="mt-6 grid grid-cols-2 gap-x-6 gap-y-4">
                 @foreach($ideas as $idea) 
-                    <a href="/ideas/{{ $idea->id }}/edit" class="text-sm">{{ $idea->description }}</a>
+                    <a href="/ideas/{{ $idea->id }}/edit" class="card bg-neutral text-neutral-content w-96">
+                        <div class="card-body">
+                            <h2 class="card-title">{{ $idea->description }}</h2>
+                        </div>
+                    </a>
                 @endforeach
             </ul>
         </div>
