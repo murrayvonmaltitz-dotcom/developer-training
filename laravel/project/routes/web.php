c9751a7dbb2b4152deec2ff2ac13849a09a046fa
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-26 15:57:41 +0200

    added access middleware to routes

--- laravel/project/routes/web.php
+++ laravel/project/routes/web.php
@@ -6,17 +6,23 @@ use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Idea;
 
-Route::get('/ideas', [IdeaController::class, 'index']);
-Route::get('/ideas/create', [IdeaController::class, 'create']);
-Route::post('/ideas', [IdeaController::class, 'store']);
-Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
-Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::get('/ideas', [IdeaController::class, 'index'])->middleware('auth');
+    Route::get('/ideas/create', [IdeaController::class, 'create']);
+    Route::post('/ideas', [IdeaController::class, 'store']);
+    Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
+    Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
+    Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
+    Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
 
-Route::get('/register', [RegisterUserController::class, 'create']);
-Route::post('/register', [RegisterUserController::class, 'store']);
+    Route::delete('/logout', [SessionsController::class, 'destroy']);
+});
 
-Route::get('/login', [SessionsController::class, 'create']);
-Route::post('/login', [SessionsController::class, 'store']);
-Route::delete('/logout', [SessionsController::class, 'destroy']);
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisterUserController::class, 'create']);
+    Route::post('/register', [RegisterUserController::class, 'store']);
+   
+    Route::get('/login', [SessionsController::class, 'create'])->name("login");
+    Route::post('/login', [SessionsController::class, 'store']);
+});
+
