5add21262adfb90d985fd2b61fe899d7fbea8549
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-26 10:40:54 +0200

    add idea edit routes and views

--- laravel/project/resources/views/ideas/index.blade.php
+++ laravel/project/resources/views/ideas/index.blade.php
@@ -2,9 +2,9 @@
     <form method="POST" action="/ideas">
         @csrf
         <div class="col-span-full">
-            <label for="idea" class="block text-sm/6 font-medium text-white">New Idea</label>
+            <label for="description" class="block text-sm/6 font-medium text-white">New Idea</label>
             <div class="mt-2">
-            <textarea id="idea" name="idea" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
+            <textarea id="description" name="description" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
             </div>
             <p class="mt-3 text-sm/6 text-gray-400">Have an idea you want to save for later.</p>
         </div>
--- laravel/project/routes/web.php
+++ laravel/project/routes/web.php
@@ -18,9 +18,12 @@ Route::get('/ideas', function () {
 });
 
 //single idea from id passing $id into the function
-Route::get('/ideas/{id}', function ($id) {
-    $idea = Idea::find($id);
-    
+//can pass a model type into function to access more directly
+Route::get('/ideas/{idea}', function (Idea $idea) {
+    //find single idea by idea
+    //$idea = Idea::find($id);
+
+    // $idea = Idea::findOrFail($id); //if id not found then display 404 page when id is passed through route function
     return view('ideas.show', [
         'idea' => $idea
     ]);
@@ -29,12 +32,30 @@ Route::get('/ideas/{id}', function ($id) {
 Route::post('/ideas', function () {
     //created_at and updated_at auto fills with eloquent 
     Idea::create([
-        'description' => request('idea'),
+        'description' => request('description'),
         'state' => 'pending'
     ]);
 
     return redirect('/');
 });
+
+//edit show fields to edit 
+Route::get('/ideas/{idea}/edit', function (Idea $idea) {
+    return view('ideas.edit', [
+        'idea' => $idea
+    ]);
+});
+
+Route::patch('/ideas/{idea}', function (Idea $idea) {
+    //update description from request from form in edit view, name of field must be description
+    $idea->update([
+        'description' => request("description")
+    ]);
+
+    return redirect("ideas/{$idea->id}");
+});
+
+
 
 Route::get('/delete-ideas', function() {
     //!!deletes everything in table
