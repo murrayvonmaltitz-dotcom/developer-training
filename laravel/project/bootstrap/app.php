deb09460e181bfce2efdabec0efc02521f358bcd
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-27 07:39:01 +0200

    default redirect users to /login

--- laravel/project/bootstrap/app.php
+++ laravel/project/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         // $middleware->redirectGuestsTo("/login");
-        $middleware->redirectUsersTo('/login');
+        $middleware->redirectUsersTo('/ideas');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
