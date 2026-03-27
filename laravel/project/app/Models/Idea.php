083569a23bb52de93d1303d0d8b00154cc8e647f
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-27 08:49:55 +0200

    define user -> idea relationships

--- laravel/project/app/Models/Idea.php
+++ laravel/project/app/Models/Idea.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Idea extends Model
 {
     //removes guarded protection
     protected $guarded = [];
     //create a one to many relationship between user and ideas 
-    public function user() {
+
+    public function user() : BelongsTo 
+    {
         return $this->belongsTo(User::class);
     }
 }
