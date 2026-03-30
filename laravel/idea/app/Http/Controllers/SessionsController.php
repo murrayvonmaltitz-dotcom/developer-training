ffca560227b38e7b0c5ff8167348ab6006d413f3
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-30 15:25:45 +0200

    tailwind and auth setup

---
+++ laravel/idea/app/Http/Controllers/SessionsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class SessionsController extends Controller
+{
+    public function create() 
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:3', 'max:255'],
+        ]);
+
+        if (!Auth::attempt($attributes)) {
+            return back()
+                ->withErrors(["password" => "we were unable to authenticate using the current credentails"])
+                ->withInput();
+        }
+
+        $request->session()->regenerate();
+           
+        return redirect()->intended()->with('success', 'You are now logge in!');
+    }
+
+    public function destroy() 
+    {
+        Auth::logout();
+
+        return redirect('/');
+    }
+}
