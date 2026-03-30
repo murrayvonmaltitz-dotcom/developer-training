7036f2dc02a2b02cb6828cb76b0fcb53c9f280ae
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-03-30 19:44:13 +0200

    testing setup

--- laravel/idea/tests/Browser/RegisterTest.php
+++ laravel/idea/tests/Browser/RegisterTest.php
@@ -2,9 +2,20 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Auth;
+
 it('registers a user', function () {
     visit('/register')
         ->fill('name', 'John Doe')
-        ->fill('email', 'John@Doe.com')
-        ->fill('password', 'password');
+        ->fill('email', 'John@example.com')
+        ->fill('password', 'password')
+        ->click('Create Account')
+        ->assertPathIs('/');
+    
+    $this->assertAuthenticated();
+
+    expect(Auth::user())->toMatchArray([
+        "name" => "John Doe",
+        "name" => "John@example.com",
+    ]);
 });
