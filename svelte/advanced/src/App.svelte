a32e5c3ec1bcfa88bb2eb493c3bf6816327f80bc
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-04-01 13:53:40 +0200

    passing snippets to components

--- svelte/advanced/src/App.svelte
+++ svelte/advanced/src/App.svelte
@@ -1,38 +1,81 @@
+<script>
+	import FilteredList from './FilteredList.svelte';
+	import { colors } from './data.js';
+</script>
 
+<FilteredList
+	data={colors}
+	field="name"
+    {header}
+    {row}
+></FilteredList>
 
-<table>
-	<thead>
-		<tr>
-			<th>emoji</th>
-			<th>description</th>
-			<th>unicode escape sequence</th>
-			<th>html entity</th>
-		</tr>
-	</thead>
-
-	<tbody>
-{#snippet monkey(emoji, description)}
-    <tr>
-        <td>{emoji}</td>
-        <td>{description}</td>
-        <td>\u{emoji.charCodeAt(0).toString(16)}\u{emoji.charCodeAt(1).toString(16)}</td>
-        <td>&amp#{emoji.codePointAt(0)}</td>
-    </tr>
+{#snippet header()}
+	<header>
+		<span class="color"></span>
+		<span class="name">name</span>
+		<span class="hex">hex</span>
+		<span class="rgb">rgb</span>
+		<span class="hsl">hsl</span>
+	</header>
 {/snippet}
 
-{@render monkey('🙈', 'see no evil')}
-{@render monkey('🙈', 'see no evil 2')}
-
-	</tbody>
-</table>
+{#snippet row(d)}
+	<div class="row">
+		<span class="color" style="background-color: {d.hex}"></span>
+		<span class="name">{d.name}</span>
+		<span class="hex">{d.hex}</span>
+		<span class="rgb">{d.rgb}</span>
+		<span class="hsl">{d.hsl}</span>
+	</div>
+{/snippet}
 
 <style>
-	th, td {
-		padding: 0.5em;
+	header, .row {
+		display: grid;
+		align-items: center;
+		grid-template-columns: 2em 4fr 3fr;
+		gap: 1em;
+		padding: 0.1em;
+		background: var(--bg-1);
+		border-radius: 0.2em;
 	}
 
-	td:nth-child(3),
-	td:nth-child(4) {
-		font-family: monospace;
+	header {
+		font-weight: bold;
+	}
+
+	.row:hover {
+		background: var(--bg-2);
+	}
+
+	.color {
+		aspect-ratio: 1;
+		height: 100%;
+		border-radius: 0.1em;
+	}
+
+	.rgb, .hsl {
+		display: none;
+	}
+
+	@media (min-width: 40rem) {
+		header, .row {
+			grid-template-columns: 2em 4fr 3fr 3fr;
+		}
+
+		.rgb {
+			display: block;
+		}
+	}
+
+	@media (min-width: 60rem) {
+		header, .row {
+			grid-template-columns: 2em 4fr 3fr 3fr 3fr;
+		}
+
+		.hsl {
+			display: block;
+		}
 	}
 </style>
