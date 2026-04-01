4a0ae297e064e7334b0a8ab4a28d1113947535c8
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-04-01 12:42:10 +0200

    class attribute

--- svelte/basic/src/routes/+page.svelte
+++ svelte/basic/src/routes/+page.svelte
@@ -1,30 +1,92 @@
-//marked component is not availible https://svelte.dev/tutorial/svelte/textarea-inputs
-
 <script>
-	import { marked } from 'marked';
-
-	let value = $state(`Some words are *italic*, some are **bold**\n\n- lists\n- are\n- cool`);
+	let flipped = $state(false);
 </script>
 
-<div class="grid">
-	input
-	<textarea bind:value={value}></textarea>
-
-	output
-	<div>{@html marked(value)}</div>
+<div class="container">
+	Flip the card
+	<button
+		class={["card", {flipped}]}
+		onclick={() => flipped = !flipped}
+	>
+		<div class="front">
+			<span class="symbol">♠</span>
+		</div>
+		<div class="back">
+			<div class="pattern"></div>
+		</div>
+	</button>
 </div>
 
 <style>
-	.grid {
-		display: grid;
-		grid-template-columns: 5em 1fr;
-		grid-template-rows: 1fr 1fr;
-		grid-gap: 1em;
+	.container {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
 		height: 100%;
+		align-items: center;
+		justify-content: center;
+		perspective: 100vh;
 	}
 
-	textarea {
-		flex: 1;
-		resize: none;
+	.card {
+		position: relative;
+		aspect-ratio: 2.5 / 3.5;
+		font-size: min(1vh, 0.25rem);
+		height: 80em;
+		background: var(--bg-1);
+		border-radius: 2em;
+		transform: rotateY(180deg);
+		transition: transform 0.4s;
+		transform-style: preserve-3d;
+		padding: 0;
+		user-select: none;
+		cursor: pointer;
+	}
+
+	.card.flipped {
+		transform: rotateY(0);
+	}
+
+	.front, .back {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		left: 0;
+		top: 0;
+		backface-visibility: hidden;
+		border-radius: 2em;
+		border: 1px solid var(--fg-2);
+		box-sizing: border-box;
+		padding: 2em;
+	}
+
+	.front {
+		background: url(../components/svelte-logo.svg) no-repeat 5em 5em, url(../components/svelte-logo.svg) no-repeat calc(100% - 5em) calc(100% - 5em);
+		background-size: 8em 8em, 8em 8em;
+	}
+
+	.back {
+		transform: rotateY(180deg);
+	}
+
+	.symbol {
+		font-size: 30em;
+		color: black;
+	}
+
+	.pattern {
+		width: 100%;
+		height: 100%;
+		background-color: var(--bg-2);
+		/* pattern from https://projects.verou.me/css3patterns/#marrakesh */
+		background-image:
+		radial-gradient(var(--bg-3) 0.9em, transparent 1em),
+		repeating-radial-gradient(var(--bg-3) 0, var(--bg-3) 0.4em, transparent 0.5em, transparent 2em, var(--bg-3) 2.1em, var(--bg-3) 2.5em, transparent 2.6em, transparent 5em);
+		background-size: 3em 3em, 9em 9em;
+		background-position: 0 0;
+		border-radius: 1em;
 	}
 </style>
