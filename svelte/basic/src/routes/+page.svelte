bef8c243147eef814ecac5743b690186ce60bfa6
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-04-01 08:23:46 +0200

    derived

--- svelte/basic/src/routes/+page.svelte
+++ svelte/basic/src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
 	let numbers = $state([1, 2, 3, 4]);
+    let total = $derived(numbers.reduce((t, n) => t + n, 0))
 
 	function addNumber() {
-		numbers.push(numbers.length + 1)
+		numbers.push(numbers.length + 1);
 	}
 </script>
 
-<p>{numbers.join(' + ')} = ...</p>
+<p>{numbers.join(' + ')} = {total}</p>
 
 <button onclick={addNumber}>
 	Add a number
