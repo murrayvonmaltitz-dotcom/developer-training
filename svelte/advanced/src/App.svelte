0e874b8eb82d934f7f5be72788318db8e2dd9a9d
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-04-01 15:24:34 +0200

    deffered transitions

--- svelte/advanced/src/App.svelte
+++ svelte/advanced/src/App.svelte
@@ -1,141 +1,68 @@
 <script>
-	import Canvas from './Canvas.svelte';
-	import { trapFocus } from './attachments.svelte.js';
+	import TodoList from './TodoList.svelte';
 
-	const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'white', 'black'];
+	const todos = $state([
+		{ id: 1, done: false, description: 'write some docs' },
+		{ id: 2, done: false, description: 'start writing blog post' },
+		{ id: 3, done: true, description: 'buy some milk' },
+		{ id: 4, done: false, description: 'mow the lawn' },
+		{ id: 5, done: false, description: 'feed the turtle' },
+		{ id: 6, done: false, description: 'fix some bugs' }
+	]);
 
-	let selected = $state(colors[0]);
-	let size = $state(10);
-	let showMenu = $state(true);
+	let uid = todos.length + 1;
 
-	let canvas;
+	function remove(todo) {
+		const index = todos.indexOf(todo);
+		todos.splice(index, 1);
+	}
 </script>
 
-<div class="container">
-	<Canvas bind:this={canvas} color={selected} size={size} />
+<div class="board">
+	<input
+		placeholder="what needs to be done?"
+		onkeydown={(e) => {
+			if (e.key !== 'Enter') return;
 
-	{#if showMenu}
-		<div
-			role="presentation"
-			class="modal-background"
-			onclick={(event) => {
-				if (event.target === event.currentTarget) {
-					showMenu = false;
-				}
-			}}
-			onkeydown={(e) => {
-				if (e.key === 'Escape') {
-					showMenu = false;
-				}
-			}}
-		>
-			<div class="menu" {@attach trapFocus}>
-				<div class="colors">
-					{#each colors as color}
-						<button
-							class="color"
-							aria-label={color}
-							aria-current={selected === color}
-							style="--color: {color}"
-							onclick={() => {
-								selected = color;
-							}}
-						></button>
-					{/each}
-				</div>
+			todos.push({
+				id: uid++,
+				done: false,
+				description: e.currentTarget.value
+			});
 
-				<label>
-					small
-					<input type="range" bind:value={size} min="1" max="50" />
-					large
-				</label>
-			</div>
-		</div>
-	{/if}
+			e.currentTarget.value = '';
+		}}
+	/>
 
-	<div class="controls">
-		<button class="show-menu" onclick={() => showMenu = !showMenu}>
-			{showMenu ? 'close' : 'menu'}
-		</button>
+	<div class="todo">
+		<h2>todo</h2>
+		<TodoList todos={todos.filter((t) => !t.done)} {remove} />
+	</div>
 
-		<button onclick={() => canvas.clear()}>
-			clear
-		</button>
+	<div class="done">
+		<h2>done</h2>
+		<TodoList todos={todos.filter((t) => t.done)} {remove} />
 	</div>
 </div>
 
 <style>
-	.container {
-		position: fixed;
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100%;
-	}
-
-	.controls {
-		position: absolute;
-		left: 0;
-		top: 0;
-		padding: 1em;
-	}
-
-	.show-menu {
-		width: 5em;
-	}
-
-	.modal-background {
-		position: fixed;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		left: 0;
-		top: 0;
-		width: 100%;
-		height: 100%;
-		backdrop-filter: blur(20px);
-	}
-
-	.menu {
-		position: relative;
-		background: var(--bg-2);
-		width: calc(100% - 2em);
-		max-width: 28em;
-		padding: 1em 1em 0.5em 1em;
-		border-radius: 1em;
-		box-sizing: border-box;
-		user-select: none;
-	}
-
-	.colors {
+	.board {
 		display: grid;
-		align-items: center;
-		grid-template-columns: repeat(9, 1fr);
-		grid-gap: 0.5em;
+		grid-template-columns: 1fr 1fr;
+		grid-column-gap: 1em;
+		max-width: 36em;
+		margin: 0 auto;
 	}
 
-	.color {
-		aspect-ratio: 1;
-		border-radius: 50%;
-		background: var(--color, #fff);
-		transform: none;
-		filter: drop-shadow(2px 2px 3px rgba(0,0,0,0.2));
-		transition: all 0.1s;
+	.board > input {
+		font-size: 1.4em;
+		grid-column: 1/3;
+		padding: 0.5em;
+		margin: 0 0 1rem 0;
 	}
 
-	.color[aria-current="true"] {
-		transform: translate(1px, 1px);
-		filter: none;
-		box-shadow: inset 3px 3px 4px rgba(0,0,0,0.2);
-	}
-
-	.menu label {
-		display: flex;
-		width: 100%;
-		margin: 1em 0 0 0;
-	}
-
-	.menu input {
-		flex: 1;
+	h2 {
+		font-size: 2em;
+		font-weight: 200;
 	}
 </style>
