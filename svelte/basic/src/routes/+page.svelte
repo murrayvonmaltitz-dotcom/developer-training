0d951e57d9902c06d47c023cad6a9e37ac9c4ca6
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-04-01 08:53:18 +0200

    inspecting state

--- svelte/basic/src/routes/+page.svelte
+++ svelte/basic/src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
 	let numbers = $state([1, 2, 3, 4]);
-    let total = $derived(numbers.reduce((t, n) => t + n, 0))
+	let total = $derived(numbers.reduce((t, n) => t + n, 0));
 
 	function addNumber() {
 		numbers.push(numbers.length + 1);
+		//console.log($state.snapshot(numbers));
 	}
+
+    $inspect(numbers).with(console.trace);
 </script>
 
 <p>{numbers.join(' + ')} = {total}</p>
