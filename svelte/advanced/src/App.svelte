1cd199b4c2bc7d846b42c65de79b3b6f410a2933
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-04-01 13:35:01 +0200

    Reactive built ins: date

--- svelte/advanced/src/App.svelte
+++ svelte/advanced/src/App.svelte
@@ -1,82 +1,18 @@
 <script>
-	const MAX_SIZE = 200;
+import {SvelteDate} from 'svelte/reactivity'
+	let date = new SvelteDate();
 
-	class Box {
-		#width = $state(0);
-		#height = $state(0);
-		area = $derived(this.#width * this.#height);
+	const pad = (n) => n < 10 ? '0' + n : n;
 
-		constructor(width, height) {
-			this.#width = width;
-			this.#height = height;
-		}
+	$effect(() => {
+		const interval = setInterval(() => {
+			date.setTime(Date.now());
+		}, 1000);
 
-        get width() {
-            return this.#width;
-        }
-
-        get height() {
-            return this.#height
-        }
-
-        set width(value) {
-            this.#width = Math.max(0, Math.min(MAX_SIZE, value));
-        }
-
-        set height(value) {
-            this.#height = Math.min(MAX_SIZE, value);
-        }
-
-		embiggen(amount) {
-			this.width += amount;
-			this.height += amount;
-		}
-	}
-
-	const box = new Box(100, 100);
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
-<label>
-	<input type="range" bind:value={box.width} min={0} max={MAX_SIZE} />
-	{box.width}
-</label>
-
-<label>
-	<input type="range" bind:value={box.height} min={0} max={MAX_SIZE} />
-	{box.height}
-</label>
-
-<button onclick={() => box.embiggen(10)}>embiggen</button>
-
-<hr>
-
-<div
-	class="box"
-	style:width="{box.width}px"
-	style:height="{box.height}px"
->
-	{box.area}
-</div>
-
-<style>
-	label {
-		display: flex;
-		align-items: center;
-	}
-
-	hr {
-		margin: 1em 0;
-		border: none;
-		border-bottom: 1px solid #888;
-	}
-
-	.box {
-		background: radial-gradient(at 25% 25%, hsl(15 100 60), hsl(15 100 50)) ;
-		border-radius: 2px;
-		filter: drop-shadow(0 0 10px hsl(15 100 50 / 0.3));
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		overflow: hidden;
-	}
-</style>
+<p>The time is {date.getHours()}:{pad(date.getMinutes())}:{pad(date.getSeconds())}</p>
