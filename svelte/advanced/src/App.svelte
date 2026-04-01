d19c53a15bfb3f20f73e788beec8f7520622e227
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-04-01 14:14:49 +0200

    added motion with tween

--- svelte/advanced/src/App.svelte
+++ svelte/advanced/src/App.svelte
@@ -1,79 +1,38 @@
 <script>
-	import FilteredList from './FilteredList.svelte';
-	import { colors } from './data.js';
+    import {Tween} from "svelte/motion";
+    import {cubicOut} from 'svelte/easing'
+
+	let progress = new Tween(0, {
+        duration: 400,
+        easing: cubicOut
+    });
 </script>
 
-<FilteredList
-	data={colors}
-	field="name"
->
-    <header>
-        <span class="color"></span>
-        <span class="name">name</span>
-        <span class="hex">hex</span>
-        <span class="rgb">rgb</span>
-        <span class="hsl">hsl</span>
-    </header>
+<progress value={progress.current}></progress>
 
-    {#snippet row(d)}
-        <div class="row">
-            <span class="color" style="background-color: {d.hex}"></span>
-            <span class="name">{d.name}</span>
-            <span class="hex">{d.hex}</span>
-            <span class="rgb">{d.rgb}</span>
-            <span class="hsl">{d.hsl}</span>
-        </div>
-    {/snippet}
-</FilteredList>
+<button onclick={() => (progress.target = 0)}>
+	0%
+</button>
 
+<button onclick={() => (progress.target = 0.25)}>
+	25%
+</button>
 
+<button onclick={() => (progress.target = 0.5)}>
+	50%
+</button>
+
+<button onclick={() => (progress.target = 0.75)}>
+	75%
+</button>
+
+<button onclick={() => (progress.target = 1)}>
+	100%
+</button>
 
 <style>
-	header, .row {
-		display: grid;
-		align-items: center;
-		grid-template-columns: 2em 4fr 3fr;
-		gap: 1em;
-		padding: 0.1em;
-		background: var(--bg-1);
-		border-radius: 0.2em;
-	}
-
-	header {
-		font-weight: bold;
-	}
-
-	.row:hover {
-		background: var(--bg-2);
-	}
-
-	.color {
-		aspect-ratio: 1;
-		height: 100%;
-		border-radius: 0.1em;
-	}
-
-	.rgb, .hsl {
-		display: none;
-	}
-
-	@media (min-width: 40rem) {
-		header, .row {
-			grid-template-columns: 2em 4fr 3fr 3fr;
-		}
-
-		.rgb {
-			display: block;
-		}
-	}
-
-	@media (min-width: 60rem) {
-		header, .row {
-			grid-template-columns: 2em 4fr 3fr 3fr 3fr;
-		}
-
-		.hsl {
-			display: block;
-		}
+	progress {
+		display: block;
+		width: 100%;
 	}
 </style>
