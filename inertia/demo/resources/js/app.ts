627741583611dff7dfe1034793062c94d4434678
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-04-06 09:53:17 +0200

    refactored to default layout

--- inertia/demo/resources/js/app.ts
+++ inertia/demo/resources/js/app.ts
@@ -1,9 +1,16 @@
 import { createInertiaApp, Link } from '@inertiajs/vue3';
 import { createApp, h } from "vue";
+import Layout from "./Shared/Layout.vue";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
+    resolve: name => {
+    const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
+    let page = pages[`./pages/${name}.vue`]
+    page.default.layout = name.startsWith('Public/') ? undefined : Layout
+    return page
+},
     setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
--- inertia/demo/resources/js/pages/Users.vue
+++ inertia/demo/resources/js/pages/Users.vue
@@ -1,28 +1,23 @@
 <template>
-    <h1 class="text-3xl">
-      Users
-    </h1>
+  <h1 class="text-3xl">
+    Users
+  </h1>
 
-    <div style="margin-top: 400px">
-      <p>The current time is {{ time }}.</p>
+  <div style="margin-top: 400px">
+    <p>The current time is {{ time }}.</p>
 
-      <Link
-        href="/users"
-        class="text-blue-500"
-        preserve-scroll
-      >
-        Refresh
-      </Link>
-    </div>
+    <Link
+      href="/users"
+      class="text-blue-500"
+      preserve-scroll
+    >
+      Refresh
+    </Link>
+  </div>
 </template>
 
 <script>
-import Layout from "../Shared/Layout.vue";
-
 export default {
-  components: { Layout },
-  layout: Layout,
-
   props: { time: String }
 };
 </script>
