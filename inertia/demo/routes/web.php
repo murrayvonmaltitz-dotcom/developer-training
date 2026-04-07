7fa4154a380ef0b3977d8983970a279c0695ba01
Murray von Maltitz <murray.vonmaltitz@stratusolve.com>
2026-04-07 07:45:34 +0200

    added form helper

--- inertia/demo/routes/web.php
+++ inertia/demo/routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
 });
 
 Route::get('/users', function () {
+    sleep(3);
     return Inertia::render('Users/Index', [
             'users' => User::query()
             ->when(Request::input('search'), function ($query, $search) {
